d29440d07416adcf97cfa6cc255d48760d5125ec
Igor <igor.rasin@gmail.com>
2022-10-08 16:17:36 +0200

    Initial Commit

---
+++ lab1/basichistogram.py
@@ -0,0 +1,75 @@
+import matplotlib.pyplot as plt
+import cv2
+from PIL import Image
+
+
+def hist_mono(source):
+
+    fig = plt.figure(figsize=(10, 7))
+    rows = 2
+    columns = 1
+
+    img = cv2.imread(source, 0)
+    # original image
+    fig.add_subplot(rows, columns, 1)
+    plt.title('Original image')
+    plt.imshow(img, cmap="gray")
+
+    # histogram of image
+    fig.add_subplot(rows, columns, 2)
+    plt.title('Histogram')
+
+    plt.hist(img.ravel(), 256, [0, 256])
+    plt.show()
+
+
+def hist_color1(source):  # openCV
+    img = cv2.imread(source)
+    fig = plt.figure(figsize=(10, 7))
+    rows = 4
+    columns = 1
+
+    # Calculate histogram without mask
+    hist1 = cv2.calcHist([img], [0], None, [256], [0, 256])
+    hist2 = cv2.calcHist([img], [1], None, [256], [0, 256])
+    hist3 = cv2.calcHist([img], [2], None, [256], [0, 256])
+
+    fig.add_subplot(rows, columns, 1)
+    plt.title('Original image')
+    plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+    fig.add_subplot(rows, columns, 2)
+    plt.plot(hist1, color='darkblue'),
+    fig.add_subplot(rows, columns, 3)
+    plt.plot(hist2, color="green"),
+    fig.add_subplot(rows, columns, 4)
+    plt.plot(hist3, color="red")
+
+    plt.show()
+
+
+def hist_color(source):  # openCV
+    img = Image.open(source)
+    fig = plt.figure(figsize=(10, 7))
+    rows = 4
+    columns = 1
+
+    # Calculate histogram without mask
+    histogram = img.histogram()
+    hist1 = histogram[0:256]
+    hist2 = histogram[256:512]
+    hist3 = histogram[512:768]
+
+    fig.add_subplot(rows, columns, 1)
+    plt.title('Original image')
+    plt.imshow(img)
+    fig.add_subplot(rows, columns, 2)
+    for i in range(0, 256):
+        plt.bar(i, hist1[i], color="red", edgecolor="red")
+    fig.add_subplot(rows, columns, 3)
+    for i in range(0, 256):
+        plt.bar(i, hist2[i], color="green", edgecolor="green")
+    fig.add_subplot(rows, columns, 4)
+    for i in range(0, 256):
+        plt.bar(i, hist3[i], color="darkblue", edgecolor="darkblue")
+
+    plt.show()
---
+++ main.py
@@ -0,0 +1,89 @@
+from lab1 import basichistogram
+import PySimpleGUI as sg
+from PIL import Image
+import shutil
+import os
+
+number = 0
+
+
+def copy_image(filename):
+    global number
+    name = os.path.basename(filename)[:-4]
+    shutil.copy(filename, f'copied/{name + str(number)}.png')
+    number += 1
+
+
+def save_image(filename):
+    name = os.path.basename(filename)[:-4]
+    img = Image.open(filename)
+    path_to = sg.popup_get_folder('file to open', no_window=True)
+    img.save(f'{path_to}/{name}.png', format='PNG')
+    print('Saved')
+
+
+def make_win1(filename):
+    im = Image.open(filename)
+    im.thumbnail(size=(900, 900))
+    im.save(filename, format="PNG")
+    layout = [[sg.Image(filename)]]
+    return sg.Window(filename, layout, location=(800, 600), resizable=True, finalize=True)
+
+
+def test_menus():
+
+    filename = 'images/Mona_Lisa_GS2.jpg'
+    sg.theme('LightGreen')
+    sg.set_options(element_padding=(0, 0))
+
+    # ------ Menu Definition ------ #
+    menu_def = [['&File', ['&Open', 'Save', 'Duplicate']],
+                ['&Lab1', ['&Histogram', ['&Monochromatic', '&Color']], ],
+                ['&Lab2', []],
+                ['&Lab3', []],
+                ['&Lab4', []],
+                ['&Lab5', []],
+                ['&Lab6', []],
+                ['&Lab7', []],
+                ['&Lab8', []], ]
+
+    right_click_menu = ['Unused', []]
+
+    # ------ GUI Defintion ------ #
+    layout = [
+        [sg.Menu(menu_def, tearoff=False, pad=(200, 1))],
+        [sg.Output(expand_x=True, expand_y=True, size=(60, 5))]
+    ]
+
+    window = sg.Window("APO",
+                       layout,
+                       default_element_size=(12, 1),
+                       default_button_element_size=(12, 1),
+                       right_click_menu=right_click_menu,
+                       resizable=True)
+
+    # ------ Loop & Process button menu choices ------ #
+    while True:
+        event, values = window.read()
+        if event in (sg.WIN_CLOSED, 'Exit'):
+            break
+        print(event, values)
+        # ------ Process menu choices ------ #
+        if event == 'Open':
+            filename = sg.popup_get_file('file to open', no_window=True)
+            make_win1(filename)
+            print(filename)
+        elif event == 'Save':
+            save_image(filename)
+        elif event == 'Duplicate':
+            copy_image(filename)
+        elif event == 'Monochromatic':
+            basichistogram.hist_mono(filename)
+        elif event == 'Color':
+            basichistogram.hist_color(filename)
+
+    window.close()
+
+
+if __name__ == '__main__':
+    test_menus()
